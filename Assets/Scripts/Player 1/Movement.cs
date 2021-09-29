0fb84d75544f95b74381ffff6abf646ff4fd24a3
Raniyafa <88967704+Raniyafa@users.noreply.github.com>
2021-09-30 11:15:49 +1300

    My first commit

---
+++ Assets/Scripts/Player 1/Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rb;
+    float horizontalInput;
+
+    public float jumpForce = 400f;
+    public LayerMask groundMask;
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        void Jump()
+        {
+            float height = GetComponent<Collider>().bounds.size.y;
+            bool isGrounded = Physics.Raycast(transform.position, Vector3.down, (height / 2) + 0.1f, groundMask);
+
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+
+    }
+}

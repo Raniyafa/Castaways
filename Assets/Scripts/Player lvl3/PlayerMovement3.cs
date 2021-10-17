f142131fa3979bf0ad5bd912e193de8c3c66fccc
Raniyafa <88967704+Raniyafa@users.noreply.github.com>
2021-10-17 19:54:27 +1300

    Added character to level 3 and added movement

---
+++ Assets/Scripts/Player lvl3/PlayerMovement3.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement3 : MonoBehaviour
+{
+    static Animator anim;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("isJumping");
+        }
+    }
+}
